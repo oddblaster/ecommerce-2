37bc24f6f40f4bfcdde7551e9ca926ed33adff7a
oddblaster <ChuyangZhangGeorge@gmail.com>
2024-03-07 19:24:18 -0500

    start cd navbar

---
+++ src/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+    {name: 'New', path: '/collection/new'},
+    {name: 'Tops', path: '/collection/tops'},
+    {name: 'Spring Break', path: '/collection/springbreak'},
+    {name: 'Dresses', path: '/collection/dresses'},
+];
+
+export default categories;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import categories from "../categories"
+import {Link} from "react-router-dom"
+
+function Navbar(){
+    return(
+        <div>
+            <ul className = "flex max-w-sm-wrap items-center">
+                {categories.map((category) => (
+                    <li key={category.name}>
+                        <Link to={category.path}>{category.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+export default Navbar;
